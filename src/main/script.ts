4f22a037a5873d1f86dc26a0a826016f96bb696c
XPRISTO <habibmisi3@gmail.com>
2024-01-15 01:55:07 +0200

    f

---
+++ src/main/script.ts
@@ -0,0 +1,99 @@
+/* eslint-disable no-continue */
+/* eslint-disable no-await-in-loop */
+import { Builder, Browser, By, until } from 'selenium-webdriver';
+import { ServiceBuilder } from 'selenium-webdriver/chrome';
+const chromedriverPath = require('chromedriver').path.replace(
+  'app.asar',
+  'app.asar.unpacked',
+);
+const serviceBuilder = new ServiceBuilder(chromedriverPath);
+export default async function sender(
+  numbers: string[],
+  message: string,
+  video?: string,
+) {
+  const logs = [];
+  try {
+    const driver = await new Builder()
+      .forBrowser(Browser.CHROME)
+      .setChromeService(serviceBuilder)
+      .build();
+    let countDone = 0;
+    try {
+      await driver.get('https://web.whatsapp.com');
+      await driver.wait(
+        until.elementLocated(
+          By.xpath("//*[contains(text(), 'Search or start new chat')]"),
+        ),
+        10000000000,
+      );
+      for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i].toString().startsWith('01')) {
+          numbers[i] = `2${numbers[i]}`;
+        }
+        await driver.get(
+          `https://web.whatsapp.com/send/?phone=${numbers[i]}&text=${message}`,
+        );
+        logs.push(
+          `https://web.whatsapp.com/send/?phone=${numbers[i]}&text=${message}`,
+        );
+
+        try {
+          if (video) {
+            const attach = await driver.wait(
+              until.elementLocated(By.xpath("//div[@title='Attach']")),
+              1000000,
+            );
+            attach.click();
+            const uploadElement = await driver.wait(
+              until.elementLocated(
+                By.css(
+                  'input[accept="image/*,video/mp4,video/3gpp,video/quicktime"]',
+                ),
+              ),
+              1000000,
+            );
+            await uploadElement.sendKeys(video);
+            await driver.sleep(2000);
+          }
+          const sendBtn = await driver.wait(
+            until.elementLocated(By.css('span[data-icon="send"]')),
+            1000000,
+          );
+          await sendBtn.click();
+          // const loading = await driver.wait(
+          //   until.elementLocated(By.css('span[data-icon="msg-time"]')),
+          //   1000000,
+          // );
+          // wait for loading element to disappear
+          // await driver.wait(until.stalenessOf(loading), 1000000);
+
+          await driver.sleep(10000);
+
+          logs.push(`number ${numbers[i]} sent`);
+          countDone += 1;
+        } catch (error) {
+          logs.push(`number ${numbers[i]} is invalid`);
+          continue;
+        }
+      }
+    } catch (error: any) {
+      const errMsg = error?.messaage || 'unexpected error occured';
+      logs.push(errMsg);
+      logs.push(error?.toString());
+      logs.push(`sent ${countDone} messages from ${numbers.length}`);
+      logs.push(`failed to send ${numbers.length - countDone} messages`);
+      await driver.quit();
+      return logs;
+    } finally {
+      await driver.quit();
+      logs.push(`sent ${countDone} messages from ${numbers.length}`);
+      logs.push(`failed to send ${numbers.length - countDone} messages`);
+      return logs;
+    }
+  } catch (e) {
+    logs.push(e?.message);
+    return logs;
+  }
+}
+
---
+++ src/renderer/App.tsx
@@ -0,0 +1,137 @@
+/* eslint-disable no-restricted-syntax */
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import {
+  Form,
+  Input,
+  Upload,
+  Button,
+  ConfigProvider,
+  theme,
+  Divider,
+  List,
+  Typography,
+} from 'antd';
+import { UploadOutlined } from '@ant-design/icons';
+import XLSX from 'xlsx';
+import React, { useEffect } from 'react';
+import logo from './assets/logo.png';
+import logoWhatsapp from './assets/whatsapplogo.png';
+
+async function readExcelFile(file: Blob): Promise<string[]> {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsArrayBuffer(file);
+    reader.onload = () => {
+      try {
+        const workbook = XLSX.read(reader.result);
+        const sheetName = workbook.SheetNames[0];
+        const sheet = workbook.Sheets[sheetName];
+        const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 }) as [
+          string,
+          string,
+        ][];
+
+        const numbersOnly = jsonData.flatMap(
+          (item: [string, string]) => item[1],
+        );
+        resolve(numbersOnly);
+      } catch (error) {
+        reject(error);
+      }
+    };
+    reader.onerror = (error) => reject(error);
+  });
+}
+function Hello() {
+  const [logs, setLogs] = React.useState<string[]>([]);
+  const onFinish = async (values: any) => {
+    values.attachment = values.attachment?.fileList[0]?.originFileObj?.path;
+    values.csv = values.csv?.fileList[0]?.originFileObj;
+    if (values.csv) {
+      const numberList = await readExcelFile(values.csv);
+      for (const item of numberList) {
+        if (item === undefined) {
+          setLogs(['Invalid CSV file']);
+          return;
+        }
+      }
+      values.csv = JSON.stringify(numberList);
+    }
+    console.log('Received values of form: ', values);
+    window.electron.ipcRenderer.sendMessage('form-submission', values);
+  };
+  useEffect(() => {
+    window.electron.ipcRenderer.on('form-submission', (arg) => {
+      setLogs(JSON.parse(arg as string));
+    });
+  }, []);
+  return (
+    <ConfigProvider
+      theme={{
+        algorithm: theme.darkAlgorithm,
+        token: {
+          fontSize: 18,
+        },
+      }}
+    >
+      <div className="logos-overlap">
+        <div className="logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="logo-wtsapp">
+          <img src={logoWhatsapp} alt="logo" />
+        </div>
+      </div>
+      <Form className="isolate" onFinish={onFinish}>
+        <Form.Item name="message">
+          <Input.TextArea placeholder="Message content" />
+        </Form.Item>
+
+        <Form.Item name="csv">
+          <Upload accept=".xlsx">
+            <Button size="large" icon={<UploadOutlined />}>
+              Upload CSV file
+            </Button>
+          </Upload>
+        </Form.Item>
+
+        <Form.Item name="attachment">
+          <Upload accept="image/*,video/*">
+            <Button size="large" icon={<UploadOutlined />}>
+              Upload Image/Video (Optional)
+            </Button>
+          </Upload>
+        </Form.Item>
+
+        <Form.Item>
+          <Button size="large" type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+      <Divider />
+      <List
+        className="loglist"
+        header={<Typography.Text strong>Logs</Typography.Text>}
+        bordered
+        dataSource={logs}
+        renderItem={(item, index) => (
+          <List.Item>
+            <Typography.Text>[{index + 1}]</Typography.Text> {item}
+          </List.Item>
+        )}
+      />
+    </ConfigProvider>
+  );
+}
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hello />} />
+      </Routes>
+    </Router>
+  );
+}
